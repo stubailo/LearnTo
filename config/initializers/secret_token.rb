0733aa1d1e3865523a000ea19206b22e1bda10f2
Brodrick <brodrick.childs@gmail.com>
2012-01-12 04:13:57 -0800

    Initial push of everything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Classroom::Application.config.secret_token = '86f43efd5b8c82d23839dce0b717075e088ea6c2cebf7c3a6c6b957c99ef1f5f81c6d1850d1df5929f567cff9beacf38ce6cb8c7686a3c64136465950618f9e5'
