8e377030750e2d8b2bcf56735a59d65c0ada19d9
Jeffrey Warren <jtwarren@mit.edu>
2012-01-16 02:05:45 -0500

    Comment plus/minus functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,39 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(params[:comment])
+    @comment.post_id = params[:post_id]
+    @comment.user_id = current_user.id
+    @comment.rating = 0
+    @comment.save
+    redirect_to root_url + "forums/"+ params[:forum_id]
+  end
+
+  def destroy
+  end
+  
+  def plus1
+    rating = Rating.where("user_id = ? AND comment_id = ?", current_user.id, params[:comment_id]).first
+    if rating != nil
+      rating.value = 1
+    else
+      rating = Rating.new(:user_id => current_user.id, :comment_id => params[:comment_id], :value => 1)
+    end
+    rating.save
+    redirect_to root_url + "forums/" + params[:forum_id]
+  end
+  
+  def minus1
+    rating = Rating.where("user_id = ? AND comment_id = ?", current_user.id, params[:comment_id]).first
+    if rating != nil
+      rating.value = -1
+    else
+      rating = Rating.new(:user_id => current_user.id, :comment_id => params[:comment_id], :value => -1)
+    end
+    rating.save
+    redirect_to root_url + "forums/" + params[:forum_id]
+  end
+end
