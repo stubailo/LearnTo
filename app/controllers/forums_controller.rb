a448cf5a262df8c30c0f4dae8334b3b7ef8458d0
Jeffrey Warren <jtwarren@mit.edu>
2012-01-18 03:51:05 -0500

    forum stuff

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,11 +1,19 @@
 class ForumsController < ApplicationController
   def show
-    @tags = []
-    Tagging.select("tag_id as 'tag_id',max(created_at) as 'created_at',taggable_type as 'taggable_type',context as 'context'").order("created_at DESC").group("tag_id").limit(6).each do |tagging|
-      @tags.push(tagging.tag)
-    end
-    @user = current_user
     @forum = Forum.find(:all, :conditions => ['id = ?', params[:id]]).first
+    @user = current_user
+    @tags = []
+    @ids = []
+    Post.select(:id).where(:forum_id => 3).each do |x|
+      @ids.push(x.id)
+    end
+    
+    Tagging.select("tag_id as 'tag_id',max(created_at) as 'created_at',taggable_type as 'taggable_type',context as 'context'")
+    .where(:taggable_type => "Post")
+    .where(:taggable_id => @ids)
+    .order("created_at DESC")
+    .group("tag_id").limit(6)
+
     if @forum != nil && @user != nil
       @post = Post.new
       @comment = Comment.new
@@ -15,3 +23,4 @@ class ForumsController < ApplicationController
     end
   end
 end
+
