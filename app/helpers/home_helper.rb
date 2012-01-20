c8cc6bb84f03c72e0e05677cce55ea1d143d5bd5
Brodrick <brodrick.childs@gmail.com>
2012-01-20 18:10:59 -0500

    Made a preliminary newsfeed :D

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,21 @@
 module HomeHelper
+
+  def get_top_three(recs)
+    i = 0
+    j = 0
+    resources = []
+    until (i > 2 || j >= recs.length)
+      rec = recs[j]
+      unless rec.hidden
+        section = rec.section
+        page = section.resource_page
+        class_room = page.class_room
+        resources << [rec, [class_room, page, section, rec]]
+        i+=1
+      end
+      j+=1
+    end
+    return resources
+  end
+
 end
