b4bd062df20298c5a0fad48aa761daf2991ab750
Jeffrey Warren <jtwarren@mit.edu>
2012-01-21 19:08:23 -0500

    work to news feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,17 @@ class HomeController < ApplicationController
 
   def index
     @posts = []
+    @announcements = []
   	if current_user
   		@user = current_user
   		@class_rooms = @user.class_rooms.sort_by { |class_room| class_room.updated_at }.reverse
-  		@user.class_rooms.each { |classroom| @posts += classroom.forum.posts.limit(6)}
-  		@posts.sort_by! { |post| post.last_updated}.reverse!
+  		@user.class_rooms.each do |classroom|
+  		  @posts += classroom.forum.posts.limit(6) 
+  		  @announcements += classroom.announcements.order('created_at DESC').limit(1)
+  		end
+  		@posts.sort_by! { |post| post.last_updated}.reverse!.first(6)
+  		@announcements.sort_by! { |a| a.created_at}.reverse!.first(6)
+  		
   	end
   end        
 
