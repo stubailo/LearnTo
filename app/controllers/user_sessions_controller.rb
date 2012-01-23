ae1e6caad3636570c71c9c3ad1c1317f5db48c39
Brodrick <brodrick.childs@gmail.com>
2012-01-23 18:48:55 -0500

    actual temp fix

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -17,20 +17,19 @@ class UserSessionsController < ApplicationController
 
   def create
     @user_session = UserSession.new(params[:user_session])
-    if @user_session.account_type == "internal"
-			if @user_session.save
-				flash[:notice] = "Sign in successful!"
-				@user = @user_session.user
-				redirect_back_or_default root_path
-			elsif @user_session.attempted_record && !@user_session.invalid_password? && !@user_session.attempted_record.active?
-				flash[:fail] = render_to_string(:partial => 'user_sessions/not_active.html.erb', :locals => { :user => @user_session.attempted_record }).html_safe
-				redirect_to :action => :new
-			else
-				render :action => :new, :locals => {:failed_login => true}
-			end
-		else
-			redirect_to authentications_path, :flash => { :fail => 'Cannot submit local login form if your account was created using facebook'}
-		end
+	if @user_session.save && @user_session.user.account_type == "internal"
+		flash[:notice] = "Sign in successful!"
+		@user = @user_session.user
+		redirect_back_or_default root_path
+	elsif @user_session
+	    @user_session.destroy
+	    redirect_to authentications_path, :flash => { :fail => 'Cannot submit local login form if your account was created using facebook'}
+	elsif @user_session.attempted_record && !@user_session.invalid_password? && !@user_session.attempted_record.active?
+		flash[:fail] = render_to_string(:partial => 'user_sessions/not_active.html.erb', :locals => { :user => @user_session.attempted_record }).html_safe
+		redirect_to :action => :new
+	else
+		render :action => :new, :locals => {:failed_login => true}
+	end		
   end
 
   def destroy
