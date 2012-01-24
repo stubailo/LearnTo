bae1b25e724f52314f79f456b4e5e1f9c0d81b22
Brodrick <brodrick.childs@gmail.com>
2012-01-24 02:02:45 -0500

    I think finished putting routes for facebook login ! :D

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   
   has_many :authentications
   
-  ACCOUNT_TYPES = ["internal", "external"]
+  ACCOUNT_TYPES = ["internal", "both", "external"]
   
   def activate!
     self.active = true
