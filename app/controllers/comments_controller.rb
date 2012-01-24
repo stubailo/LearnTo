47bac7f94487a9192d4e1ed859bf2ca1fbe9d08a
Jeffrey Warren <jtwarren@mit.edu>
2012-01-24 06:21:25 -0500

    fix announcement title link

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,6 +24,7 @@ class CommentsController < ApplicationController
       else
         format.html  { render :back }
         format.json  { render :json => @comment.errors, :status => :unprocessable_entity }
+      end
     end
   end
 
