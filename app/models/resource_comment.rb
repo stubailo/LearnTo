aac87a6615ffe266d39c50e7a6a0b35acd8076d3
Jeffrey Warren <jtwarren@mit.edu>
2012-01-27 00:49:14 -0500

    working backend for resource comments

--- app/models/resource_comment.rb
+++ app/models/resource_comment.rb
@@ -1,4 +1,7 @@
 class ResourceComment < ActiveRecord::Base
   belongs_to :resource
   belongs_to :user
+  validates :resource_id, :presence => true
+  validates :user_id, :presence => true
+  validates :content, :presence => true
 end
