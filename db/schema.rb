e7487f8237897b5e216c2140d52f8e071ebe370d
Jeffrey Warren <jtwarren@mit.edu>
2012-01-27 00:37:21 -0500

    added resource comments shit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120126103531) do
+ActiveRecord::Schema.define(:version => 20120127053422) do
 
   create_table "announcements", :force => true do |t|
     t.integer  "user_id"
@@ -105,6 +105,17 @@ ActiveRecord::Schema.define(:version => 20120126103531) do
     t.datetime "updated_at",   :null => false
     t.datetime "last_updated"
   end
+
+  create_table "resource_comments", :force => true do |t|
+    t.integer  "resource_id"
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "resource_comments", ["resource_id"], :name => "index_resource_comments_on_resource_id"
+  add_index "resource_comments", ["user_id"], :name => "index_resource_comments_on_user_id"
 
   create_table "resource_pages", :force => true do |t|
     t.string   "section"
