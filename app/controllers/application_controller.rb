ab1b319a587791028e7dcbd2b02227f088af8b6d
Jeffrey Warren <jtwarren@mit.edu>
2012-01-28 04:07:03 -0500

    working notification shit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,55 +6,55 @@ class ApplicationController < ActionController::Base
   private
     before_filter :global_vars
 
-    def global_vars
-      @website_name = "LearnTo"
+  def global_vars
+    @website_name = "LearnTo"
+  end
+  
+  def require_enrolled(class_room)
+    user = current_user
+    user_permission = current_user ? UserPermission.where("user_id = ? AND class_room_id = ?", user.id, class_room.id).first.try(:permission_type) : "none"
+    unless is_creator(class_room) || user_permission == "student"
+      flash[:fail] = "You must be enrolled to view this page"
+      redirect_back_or_default class_room
     end
-    
-    def require_enrolled(class_room)
+  end
+  
+  def is_enrolled(class_room)
+    if current_user
       user = current_user
-      user_permission = current_user ? UserPermission.where("user_id = ? AND class_room_id = ?", user.id, class_room.id).first.try(:permission_type) : "none"
-      unless is_creator(class_room) || user_permission == "student"
-        flash[:fail] = "You must be enrolled to view this page"
-        redirect_back_or_default class_room
-      end
+      user_permission = UserPermission.where("user_id = ? AND class_room_id = ?", user.id, class_room.id).first.try(:permission_type)
+      return is_creator(class_room) || user_permission == "student"
+    else
+      return false
     end
-    
-    def is_enrolled(class_room)
-      if current_user
-        user = current_user
-        user_permission = UserPermission.where("user_id = ? AND class_room_id = ?", user.id, class_room.id).first.try(:permission_type)
-        return is_creator(class_room) || user_permission == "student"
-      else
-        return false
-      end
+  end
+  
+  def current_user_session
+    logger.debug "ApplicationController::current_user_session"
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+  
+  def current_user
+    logger.debug "ApplicationController::current_user"
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.user
+  end
+  
+  def require_user
+    logger.debug "ApplicationController::require_user"
+    unless current_user
+      store_location
+      flash[:fail] = "You must be logged in to access this page."
+      redirect_to login_path
+      return false
     end
-    
-    def current_user_session
-      logger.debug "ApplicationController::current_user_session"
-      return @current_user_session if defined?(@current_user_session)
-      @current_user_session = UserSession.find
-    end
-    
-    def current_user
-      logger.debug "ApplicationController::current_user"
-      return @current_user if defined?(@current_user)
-      @current_user = current_user_session && current_user_session.user
-    end
-    
-    def require_user
-      logger.debug "ApplicationController::require_user"
-      unless current_user
-        store_location
-        flash[:fail] = "You must be logged in to access this page."
-        redirect_to login_path
-        return false
-      end
-    end
-    
-    def is_creator(item)
-      user = current_user
-      return current_user && user.id == item.user_id
-    end
+  end
+  
+  def is_creator(item)
+    user = current_user
+    return current_user && user.id == item.user_id
+  end
   
   def set_vars
     @resource_pages = @class_room.resource_pages.sort_by {|x| x.id}
@@ -77,17 +77,17 @@ class ApplicationController < ActionController::Base
     end
   end  
   
-  def class_notification(action, note_type, class_room, item_id)
+  def class_notification(action, item_type, class_room, item_id)
     #note type must be post (for now) then resource
     class_room.users.each do |user|
-      notification = Notification.new(:action => action, :item_type => note_type, :user_id => user.id, :dirty => false, :item_id => item_id)
+      notification = Notification.new(:action => action, :item_type => item_type, :user_id => user.id, :read => false, :item_id => item_id)
       notification.save
     end
   end
   
-  def user_notification(action, note_type, user, item_id)
+  def user_notification(action, item_type, user, item_id)
     #note type must be post (for now) then resource
-    notification = Notification.new(:action => action, :item_type => note_type, :user_id => user.id, :dirty => false, :item_id => item_id)
+    notification = Notification.new(:action => action, :item_type => item_type, :user_id => user.id, :read => false, :item_id => item_id)
     notification.save
   end
 
@@ -102,7 +102,11 @@ class ApplicationController < ActionController::Base
   end
   
   def get_notifications
-    current_user.notifications.al
+    @notifications = current_user.notifications
+    .select('action, max(user_id) as user_id, read, item_type, item_id, count(*) as count, max(created_at) as created_at')
+    .group('action, read, item_type, item_id')
+    .order('read')
+    .order('created_at DESC')
   end
 
   def store_location
