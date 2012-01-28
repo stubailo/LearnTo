86b28927bd59514b2ea91620cfb6fce7eb482bf2
Jeffrey Warren <jtwarren@mit.edu>
2012-01-28 04:45:51 -0500

    testing justin tv

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(:version => 20120128075716) do
 
   create_table "announcements", :force => true do |t|
