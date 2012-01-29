3237c00ab1187c3c982435b897302b258b2c336e
Jeffrey Warren <jtwarren@mit.edu>
2012-01-29 06:21:38 -0500

    fixed editing other users posts and comments

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,6 +5,10 @@ class NotificationsController < ApplicationController
   
   def notifications
     user_notifications
-    render :layout => false
+    respond_to do |format|
+      partial = render_to_string "notifications/notifications.html.haml"
+      format.json {render :json => partial}
+      format.html {render :layout => false}
+    end
   end
 end
